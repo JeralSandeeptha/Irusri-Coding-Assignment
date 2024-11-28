ce39d415f4763e7817ef02e55c74b890d954be3b
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-11-29 05:01:38 +0530

    Create NotFound page and responsive it

--- src/pages/notfound-page/NotFoundPage.tsx
+++ src/pages/notfound-page/NotFoundPage.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import { Button, Chip } from '@mui/material';
 import { NotFoundPageProps } from '../../types/pages'
 import './NotFoundPage.scss';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = (props: NotFoundPageProps) => {
+
+  const navigate = useNavigate();
+
+  const goBack = (e: any) => {
+    e.preventDefault();
+    navigate(-1);
+  }
     
   return (
-    <div>
-      
+    <div className='not-found'>
+      <div className="not-found-inner">
+        <div className="badge">
+          <h5 className="error-text">404 error</h5>
+        </div>
+        <h1 className="header">We've lost this page</h1>
+        <h5 className="subheader">Sorry, the page you are looking for doesn't exist or has been moved</h5>
+        <Button variant="contained" size="small" className='control-button' onClick={goBack}>Back</Button>
+      </div>
     </div>
   );
 
