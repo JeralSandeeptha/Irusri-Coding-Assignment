b6165624f71d329eb4f6224b982a1a7082ad89ee
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-12-03 13:48:46 +0530

    Add protected routes

--- src/routes/MainNavigation.tsx
+++ src/routes/MainNavigation.tsx
@@ -1,17 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/login-page/LoginPage';
 import RegisterPage from '../pages/register-page/RegisterPage';
 import CartPage from '../pages/cart-page/CartPage';
 import ProductsPage from '../pages/products-page/ProductsPage';
 import NotFoundPage from '../pages/notfound-page/NotFoundPage';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 const MainNavigation = () => {
+
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={ProductsPage}/>
-        <Route path='/login' Component={LoginPage}/>
-        <Route path='/register' Component={RegisterPage}/>
+        <Route
+          path="/login"
+          element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!isLoggedIn ? <RegisterPage /> : <Navigate to="/" />}
+        />
         <Route path='/cart' Component={CartPage}/>
         <Route path='*' Component={NotFoundPage}/>
       </Routes>
